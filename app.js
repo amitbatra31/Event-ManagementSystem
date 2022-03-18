df771080847e65710cd68dd57f4b1e22a90ae134
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2022-03-18 23:52:07 +0530

    Add pages

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  req.render("home");
+  res.render("home", { title: "Event Mangement System" });
 });
-
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Register" });
+});
 app.listen(PORT, function (req, res) {
   console.log("Server started on port " + PORT);
 });
