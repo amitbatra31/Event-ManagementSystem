ab4682eb71eca7fb971bad6db0b21f752b0c0f54
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2022-03-20 12:08:25 +0530

    Add List,faqs,hosting form

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register", { title: "Register" });
 });
+app.get("/list", (req, res) => {
+  res.render("list", { title: "List" });
+});
+app.get("/eventreg", (req, res) => {
+  res.render("eventRegister", { title: "Register Event" });
+});
+app.get("/faq", (req, res) => {
+  res.render("faq", { title: "FAQ's" });
+});
 app.listen(PORT, function (req, res) {
   console.log("Server started on port " + PORT);
 });
